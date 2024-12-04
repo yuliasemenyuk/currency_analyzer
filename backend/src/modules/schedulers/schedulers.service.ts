cd2b3b7a9e55431c9ed5e253584316d6f249b3b6
yuliasemenyuk <semenyukyp@gmail.com>
2024-12-04 17:35:10 +0200

    add rule satisfaction check

--- backend/src/modules/schedulers/schedulers.service.ts
+++ backend/src/modules/schedulers/schedulers.service.ts
@@ -37,23 +37,64 @@ export class SchedulerService {
         this.logger.log(`Using cached rate for ${cacheKey}: ${cachedRate}`);
       } else {
         await this.redis.set(cacheKey, rate.toString(), 'EX', 600); // 10 minutes
-
-        await this.prisma.currencyRateHistory.create({
-          data: {
-            currencyPair: { connect: { id: rule.pairId } },
-            rate,
-          },
-        });
-
         this.logger.log(`Fetched new rate for ${cacheKey}: ${rate}`);
-
-        rule.users.forEach((userOnRule) => {
-          const email = userOnRule.user.email;
-          this.logger.log(
-            `Currency pair: ${rule.currencyPair.fromCode} to ${rule.currencyPair.toCode}, Rate: ${rate}, Subscribed User: ${email}`,
-          );
-        });
       }
+
+      await this.prisma.currencyRateHistory.create({
+        data: {
+          currencyPair: { connect: { id: rule.pairId } },
+          rate,
+        },
+      });
+
+      rule.users.forEach(async (userOnRule) => {
+        const email = userOnRule.user.email;
+        const isSatisfied = await this.checkRuleSatisfaction(
+          rate,
+          rule.percentage,
+          rule.trendDirection,
+          rule.pairId,
+        );
+
+        this.logger.log(
+          `Currency pair: ${rule.currencyPair.fromCode} to ${rule.currencyPair.toCode}, Rate: ${rate}, Subscribed User: ${email}, Rule satisfied: ${isSatisfied}`,
+        );
+      });
     }
+  }
+
+  private async checkRuleSatisfaction(
+    currentRate: number,
+    percentage: number,
+    trendDirection: string,
+    pairId: string,
+  ): Promise<boolean> {
+    const previousRate = await this.getPreviousRate(pairId);
+    if (!previousRate) return false;
+
+    console.log('currentRate', currentRate);
+    console.log('previousRate', previousRate);
+
+    const percentageChange = Math.round(
+      ((currentRate - previousRate) / previousRate) * 100,
+    );
+    console.log('actual percentage change', percentageChange + '%');
+    console.log('target percentage', percentage + '%');
+    console.log('direction', trendDirection);
+
+    return trendDirection === 'increase'
+      ? percentageChange >= percentage
+      : percentageChange <= -percentage;
+  }
+
+  async getPreviousRate(pairId: string): Promise<number | null> {
+    const previousRateRecord = await this.prisma.currencyRateHistory.findFirst({
+      where: { pairId },
+      orderBy: { timestamp: 'desc' },
+      skip: 1,
+    });
+
+    console.log('previousRateRecord', previousRateRecord);
+    return previousRateRecord ? previousRateRecord.rate : null;
   }
 }
